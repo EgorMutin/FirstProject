9b19d80a45c09134cecc31ff4d4b0f3673f91b8b
Igor <Igor@DESKTOP-8BG7AII>
2024-07-07 16:23:19 +0300

    Добавьте файлы проекта.

---
+++ FirstProject/Program.cs
@@ -0,0 +1,28 @@
+﻿string FullName;
+int Age;
+string Email;
+float ProgrammigScore;
+float MathScore;
+float PhysScore;
+
+
+FullName = ("Мутин Егор Николаевич");
+Age = (23);
+Email = ("mutin.egor@gmail.com");
+ProgrammigScore = (76.3F);
+MathScore = (68.3F);
+PhysScore = (73.4F);
+
+string pattern = "ФИО: {0} \nВозраст: {1} \nЭлектронная почта: {2} \nБаллы по программированию: {3} \nБаллы по математике: {4} \nБаллы по физике: {5}";
+
+   
+
+Console.WriteLine(pattern, 
+    FullName, 
+    Age,
+    Email,
+    ProgrammigScore,
+    MathScore,
+    PhysScore);
+Console.ReadLine();
+
